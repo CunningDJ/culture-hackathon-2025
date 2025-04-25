a586783f87b6b9b406f70427a225d92a80919004
Bernie Kravitz <bernie.kravitz@gmail.com>
2025-04-25 15:22:44 -0400

    WIP generating SVG

--- apps/sdk-app/src/DrawMe.tsx
+++ apps/sdk-app/src/DrawMe.tsx
@@ -1,37 +1,52 @@
 import { useDocument, useProjection } from '@sanity/sdk-react'
 import { Excalidraw, MainMenu } from "@excalidraw/excalidraw";
 import "@excalidraw/excalidraw/index.css";
- 
-const documentHandle = { 
-    "documentId": "175e1ba4-ba5b-4400-a14a-1c70459e16b9", 
+import { useParams } from 'react-router';
+import { useState } from 'react';
+import { exportToSvg } from "@excalidraw/excalidraw";
+
+export function DrawMe() {
+  const { artworkId } = useParams();
+  const [excalidrawAPI, setExcalidrawAPI] = useState(null);
+
+  const documentHandle = { 
+    "documentId": artworkId,
     "documentType": "artwork", 
     "dataset": "team-4", 
     "projectId": "n7xckjbt"
-}
+ }
 
-export function DrawMe() {
-  const id = '175e1ba4-ba5b-4400-a14a-1c70459e16b9'
-  const doc = useDocument(documentHandle)
   const proj = useProjection({
     ...documentHandle,
     projection: `{
         "images": images[0].asset->url
     }`
   })
-  //console.log("DOC", doc)
-  console.log("PROJ", proj)
   return (
     <div className="example-container">
         <h1>DrawMe</h1>
       <img src={proj?.data.images} style={{height: "500px"}}/>
       <div style={{ height: "500px", width: "100%" }}>
-        <Excalidraw >
+        <Excalidraw excalidrawAPI={(api) => setExcalidrawAPI(api)}>
             <MainMenu >
             <MainMenu.Item onSelect={() => window.alert("nice!")}>
                 Save to Public Gallery
           </MainMenu.Item> 
             </MainMenu>
         </Excalidraw>
+        <button onClick={async () => {
+         if (!excalidrawAPI) {
+            return
+        }
+        const elements = excalidrawAPI.getSceneElements();
+        if (!elements || !elements.length) {
+            return
+        }
+        const svg = await exportToSvg({elements});
+        console.log("SVG", svg)
+        }}>
+          Save
+        </button>
       </div>
     </div>
   )
