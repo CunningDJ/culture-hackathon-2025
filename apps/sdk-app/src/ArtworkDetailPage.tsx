c553c523538ae4bfcc963868c7a02a3fecd550eb
kengo <kikedaiy@gmail.com>
2025-04-25 16:00:41 -0400

    Artwork detail page.

--- apps/sdk-app/src/ArtworkDetailPage.tsx
+++ apps/sdk-app/src/ArtworkDetailPage.tsx
@@ -1,6 +1,7 @@
 import { useProjection } from "@sanity/sdk-react";
 import { Link, useParams } from "react-router";
-import {PortableText} from '@portabletext/react'
+import { IconArrow } from "./IconArrow";
+import {PortableText, PortableTextComponents} from '@portabletext/react'
 
 
 const createDocumentHandle = (documentId: string) => ({
@@ -9,6 +10,18 @@ const createDocumentHandle = (documentId: string) => ({
   dataset: "team-4",
   projectId: "n7xckjbt"
 });
+
+const pTag = ({children, value}) => {
+  // `value` is the single Portable Text block of this header
+  return <p className="typography $typography/size:large $typography/weight:regular">{children}</p>
+}
+
+
+const components: PortableTextComponents = {
+  block: {
+    normal: pTag,
+  },
+}
 
 const ArtworkDetailPage = () => {
   const { artworkId } = useParams();
@@ -31,22 +44,30 @@ const ArtworkDetailPage = () => {
   });
 
   console.log('artwork data:', doc);
+
+
   return (
-    <div>
-      <h1>{doc.name}</h1>
-      <img src={doc.imageUrl} alt={doc.name} />
+    <div className="color/background $color/background:black layout/wrapper layout/height:100% $color/white layout/flex:column layout/justify:between layout/padding:page">
       <div>
         <div>
-          <p><b>Artist:</b> {doc.artist.name}</p>
+          <h1 className="$typography/size:extra-extra-large typography"><em>{doc.name}</em></h1>
+          <img className="layout/width:100% layout/height:100% layout/margin:top:page"
+          style={{
+            height: '50vh',
+            objectFit: 'cover'
+          }}
+          src={doc.imageUrl} alt={doc.name} />
         </div>
-        <p><b>Year:</b> {doc.year}</p>
-        <p><b>Medium:</b> {doc.medium}</p>
-        <div>
-          <h2>Description</h2>
-          <PortableText value={doc.description} />
+
+        <div className="layout/margin:top:page">
+          <p className="typography $typography/size:extra-large $typography/weight:regular">{doc.artist.name}</p>
+          <p className="typography $typography/size:medium-large $typography/weight:regular layout/margin:top:cap">{doc.year}</p>
+          <div className="layout/margin:top:page"><PortableText components={components} value={doc.description} /></div>
         </div>
+      </div>
+      <div className="">
         <Link to={`/artwork/${artworkId}/draw`}>
-          <button>Now Draw!</button>
+          <span className="typography $typography/size:30pt">Start my drawing <IconArrow /></span>
         </Link>
       </div>
     </div>
