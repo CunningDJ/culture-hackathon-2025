6df2b121a0da5c2acd085b881ec50cd56d24a7b4
Anthony Hersey <anthony.hersey+github@gmail.com>
2025-04-25 15:18:46 -0400

    Ah/user drawings (#7)
    * adding userDrawing type
    
    * link type
    
    * polish up
    
    * add to UI

--- apps/studio/schemaTypes/index.ts
+++ apps/studio/schemaTypes/index.ts
@@ -1,6 +1,7 @@
-import {artworkType} from './artwork'
-import {exhibitionType} from './exhibition'
-import {newsType} from './news'
-import {personType} from './person'
+import { artworkType } from './artwork'
+import { exhibitionType } from './exhibition'
+import { newsType } from './news'
+import { personType } from './person'
+import { userDrawingType } from './userDrawing'
 
-export const schemaTypes = [personType, artworkType, exhibitionType, newsType]
+export const schemaTypes = [personType, artworkType, exhibitionType, newsType, userDrawingType]
